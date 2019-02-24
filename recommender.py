26bbe8bab19aeb150c3702f9b68e4c70bfe84cd9
Jack Foltz <jack@foltz.io>
2019-02-24 12:42:32 -0500

    Clean up stuff for demo

--- recommender.py
+++ recommender.py
@@ -1,71 +1,86 @@
 import pandas as pd
 import numpy as np
 import multiprocessing as mp
+import os.path
 import math
 import time
+import sys
+import pickle
 
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.decomposition import NMF
+
+n_items = 5000
+bar_width = 50
+bar_interval = n_items // bar_width
 
 activity = pd.read_csv('activity_data.csv')
-activity = activity.iloc[:1000]
+activity = activity.iloc[:n_items]
 hotel = pd.read_csv('hotel_data.csv')
 
-print('Calculating uniques')
 user_ids = activity.user_id.unique()
 hotel_ids = hotel.hotel_id.unique()
 
-print('Building engagement placeholder thing')
-engagement = pd.DataFrame(0, index=user_ids, columns=hotel_ids)
-print(engagement)
-print('Done')
 
-print('???')
-start = time.time()
-for i, entry in activity.iterrows():
-    #print("entry user id", entry.user_id)
-    #print("entry hotel id", entry.hotel_id)
-    engagement.loc[entry.user_id,entry.hotel_id] = 1
-#print(engagement)
-end = time.time()
-print(end - start)
+engagement = None
+if not os.path.isfile('engagement.pickle'):
+    sys.stdout.write("Building Engagement Matrix... [%s]" % (" " * bar_width))
+    sys.stdout.flush()
+    sys.stdout.write("\b" * (bar_width+1))
+
+    start = time.time()
+
+    engagement = pd.DataFrame(0, index=user_ids, columns=hotel_ids)
+    for i, entry in activity.iterrows():
+        if i % bar_interval == 0:
+            sys.stdout.write('#')
+            sys.stdout.flush()
+        engagement.loc[entry.user_id,entry.hotel_id] = 1
+    sys.stdout.write('\n')
+
+    end = time.time()
+    print("Finished in ", end - start, "s\n")
+
+    engagement.to_pickle('engagement.pickle')
+
+else:
+    engagement = pd.read_pickle('engagement.pickle')
+
+
 
 hotel_similarities = cosine_similarity(engagement.transpose())
-print(hotel_similarities)
-from sklearn.decomposition import NMF
-n = 5
+n = 15
 model = NMF(n_components=n, init='random', random_state=0)
 W = model.fit_transform(engagement)
 H = model.components_
 user_hotel_score = np.matmul(W,H)
 
+
+
 # check precision by comparing with the actual hotels they picked/viewed/booked/engaged with?
 # how do we specify what type of engagement?
-#look at the rest of the data to predict one and then compare with actual data 
-def top(idx):
-    # help
-    #print(np.sort(np.trim_zeros(user_hotel_score[idx])))
-    trimmedList = np.trim_zeros(user_hotel_score[idx])
-    
-    print("User: ", idx)
-    print("User id: " , engagement.index.values.tolist()[idx])
-    argSorted = np.argsort(trimmedList)
-    top3 = argSorted[-5:]
-    
-    for i in range(len(top3)): 
-        
-        reverseIterTop3Index = len(top3)-i-1
-        
-        cosineSimilarityTop3Index = top3[reverseIterTop3Index]
-        cosineSimilarityValue = trimmedList[cosineSimilarityTop3Index]
-        print(engagement.columns.values.tolist()[cosineSimilarityTop3Index])
-        #print(engagement.index.values.tolist())
-        print(str(i+1)+")", cosineSimilarityValue)
-        #can use index to get names of the hotels too... 
-        #print(engagement.loc[idx, i])
-        
-    
-    
+#look at the rest of the data to predict one and then compare with actual data
+def top(idx, n):
+    user_id = engagement.index.values.tolist()[idx]
+    print("Top Hotels for user_id" , user_id)
 
-print(top(14))
+    argSorted = np.argsort(user_hotel_score[idx])
+    top_n = argSorted[-n:]
 
-print('Profit')
+    for i in range(len(top_n)):
+
+        reverseIterTop_NIndex = len(top_n)-i-1
+
+        cosineSimilarityTop_NIndex = top_n[reverseIterTop_NIndex]
+        cosineSimilarityValue = user_hotel_score[idx][cosineSimilarityTop_NIndex]
+
+        hotel_id = engagement.columns.values.tolist()[cosineSimilarityTop_NIndex]
+        hotel_row = hotel.loc[hotel.hotel_id == hotel_id]
+
+        print(str(i + 1) + ")", hotel_row.hotel_name.item(), "(" + str(cosineSimilarityValue) + ")")
+        #print(activity.loc[activity.hotel_id == hotel_id])
+        #print(activity.loc[activity.user_id == user_id])
+
+
+
+top(14, 5)
